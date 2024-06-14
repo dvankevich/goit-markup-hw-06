85c1b8e5962d7fadb7353a5a55efc303efdc3e82
dvankevich <dvankevich@gmail.com>
2024-06-14 14:19:37 +0300

    add open/close mobile menu functionality

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[open-modal]"),
-    
     closeModalBtn: document.querySelector("[close]"),
     modal: document.querySelector("[modal]"),
+
+    openMobileBtn: document.querySelector("[open-mobile-menu]"),
+    closeMobileBtn: document.querySelector("[close-mobile-menu]"),
+    mmenu: document.querySelector("[mobile-menu]"),
   };
+
   refs.openModalBtn.addEventListener("click", toggleModal);
- 
   refs.closeModalBtn.addEventListener("click", toggleModal);
+
   function toggleModal() {
     refs.modal.classList.toggle("is-open");
+  }
+
+  refs.openMobileBtn.addEventListener("click", toggleMobile);
+  refs.closeMobileBtn.addEventListener("click", toggleMobile);
+
+  function toggleMobile() {
+    refs.mmenu.classList.toggle("is-open");
   }
 })();
